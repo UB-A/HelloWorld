88fe3b7e3af8ee938e835114bf920a4607320d16
Ubong Benjamin <ubongben816@gmail.com>
2022-06-25 17:14:26 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+      static void Main(string[] args)
+        {
+            Console.Write("Enter our Name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            //Console.WriteLine("Hello Usen!");
+            //Console.WriteLine("Hello Nsisong!");
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
